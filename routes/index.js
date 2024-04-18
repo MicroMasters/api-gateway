51b3e3646ea95ec6d7f524021282a44b768ec814
MadhushaPrasad <madushaprasad21@gmail.com>
2024-04-18 12:15:41 +0530

    fix(routes): apiAlreadyExists function

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,7 @@ const registry = require("./registry.json");
 const fs = require("fs");
 
 router.all("/:apiName/:path/:service/:route", (req, res) => {
-  console.log(req.params.apiName);
   if (registry.services[req.params.apiName]) {
-    console.log(
-      `${registry.services[req.params.apiName].url}/${req.params.path}/${
-        req.params.service
-      }/${req.params.route}`
-    );
-
-    console.log({
-      method: req.method,
-      url: `${registry.services[req.params.apiName].url}/${req.params.path}/${
-        req.params.service
-      }/${req.params.route}`,
-      headers: req.headers,
-      data: req.body,
-    });
-
     axios({
       method: req.method,
       url: `${registry.services[req.params.apiName].url}/${req.params.path}/${
@@ -30,8 +14,6 @@ router.all("/:apiName/:path/:service/:route", (req, res) => {
       headers: req.headers,
       data: req.body,
     }).then((response) => {
-      console.log(response);
-      console.log(response.data);
       res.send(response.data);
     });
   } else {
@@ -50,26 +32,49 @@ router.post("/register", (req, res) => {
     registrationInfo.port +
     "/";
 
-  registry.services[registrationInfo.apiName] = { ...registrationInfo };
+  if (apiAlreadyExists(registrationInfo)) {
+    res.send(
+      "API already exists for " +
+        registrationInfo.apiName +
+        " at " +
+        registrationInfo.url +
+        "\n"
+    );
+    return;
+  } else {
+    registry.services.registryServices.push({
+      ...registrationInfo,
+    });
 
-  fs.writeFile("./routes/registry.json", JSON.stringify(registry), (error) => {
-    if (error) {
-      res.send("Could not register" + registrationInfo.apiName + "\n" + error);
-    } else {
-      res.send("Successfully registered '" + registrationInfo.apiName + "");
-    }
-    I;
-  });
+    fs.writeFile(
+      "./routes/registry.json",
+      JSON.stringify(registry),
+      (error) => {
+        if (error) {
+          res.send(
+            "Could not register" + registrationInfo.apiName + "\n" + error
+          );
+        } else {
+          res.send("Successfully registered '" + registrationInfo.apiName + "");
+        }
+      }
+    );
+  }
 });
 
 const apiAlreadyExists = (registrationInfo) => {
   let exists = false;
-  registry.services[registrationInfo.apiName].forEach((instance) => {
-    if (instance.url === registrationInfo.url) {
-      exists = true;
-      return I;
-    }
-  });
+
+  if (registry.services.registryServices.length > 0) {
+    registry.services.registryServices.forEach((service) => {
+      if (
+        service.host === registrationInfo.host &&
+        service.port === registrationInfo.port
+      ) {
+        exists = true;
+      }
+    });
+  }
   return exists;
 };
 
